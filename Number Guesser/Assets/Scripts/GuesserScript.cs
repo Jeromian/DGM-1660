61e673ae60f61dee9275768e20242d023c736342
Unknown <matthewgasser@outlook.com>
2018-03-08 18:31:45 -0700

    hope this fixes everything

---
+++ Number Guesser/Assets/Scripts/GuesserScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuesserScript : MonoBehaviour {
+
+        public int max;
+        public int min;
+    public int Guess;
+        
+
+	// Use this for initialization
+	private void Start () {
+
+        
+
+        print("Welcome, you Fig Maggot!");
+        print("pick a number between "+min+" and "+max);
+
+        //Is value Guess
+        print("is it "+Guess+"?");
+        //push buttons up/down
+        print("press the up button for higher, the down button for lower, or the Spacebar to confirm.");
+
+       
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = Guess;
+            Guess = (min + max) / 2;
+            print("Is it" + Guess);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = Guess;
+            Guess = (min + max) / 2;
+            print("Is it" + Guess);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            print("HAHA I KNEW IT WAS " + Guess+"!");
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-DGM 1660
+Git
+
+
+Hi, I'm Matt! I am here to become an animator. I like video games, but im not sure i want to develoup them for a living.
